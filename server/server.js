57d161818f61d5f6d3d5f501acf2199631067263
rhubarb414 <ctophr.dubois@gmail.com>
2023-05-06 20:48:27 -0700

    create resolvers and typeDefs files

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const { ApolloServer } = require('apollo-server-express');
 
 // const { typeDefs, resolvers } = require('./schemas'); TK
 
-const routes = require('./routes'); // delete
+// const routes = require('./routes'); // delete
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -24,7 +24,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
